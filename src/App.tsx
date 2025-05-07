5b8eda95d5b5b74fccc19e3f3216767f8d7e19af
DuncanAngel <150028450+DuncanAngel@users.noreply.github.com>
2025-05-07 14:16:22 +0200

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './css/App.css'
+import './components/login'
+import Login from './components/login'
 
 function App() {
 
 
   return (
     <>
-
+      <Login />
     </>
   )
 }
---
+++ src/components/login.tsx
@@ -0,0 +1,13 @@
+import './../css/App.css'
+
+function Login() {
+
+
+  return (
+    <>
+    <div className='text-yellow-300 font-bold'>Hello</div>
+    </>
+  )
+}
+
+export default Login
